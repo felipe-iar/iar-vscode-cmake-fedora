8efcfe83c42f316bf005f4a4bace0cd9128a11d7
Felipe Torrezan <felipe.torrezan@iar.com>
2022-08-11 18:00:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
-   
-void main() {
-   while (1) {
-     printf("Hello world!\n");
-   }
+#include <intrinsics.h>
+
+__root char table[5];
+
+int foo(int a, int b)
+{
+  return a + b;
+}
+
+int main(void)
+{
+  table[0] = 10;
+  table[1] = 15;
+  table[4] = foo(table[0], table[1]);
+  while (1) {
+    __no_operation();
+  }
 }
